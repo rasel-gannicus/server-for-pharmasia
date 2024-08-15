184549aded436eb4be34cabb4794d91e9a1cc0a0
shafik720 <shafikrasel5@gmail.com>
2024-08-15 15:19:12 +0600

    adding product to database

---
+++ README.md
@@ -0,0 +1,60 @@
+# Server for Cotton Club
+This is the server code for [Multi Stage form for visiting Mars](https://github.com/rasel-gannicus/Multi-stage-form-for-visiting-Mars-application) project. 
+
+## Installation
+
+1. Clone the repository.
+2. Install dependencies using `npm install`.
+3. Rename `env.example` to `.env`.
+4. Run the server using `npm run dev`.
+
+## Configuration
+
+### Environment Variables
+
+- `MONGODB_URI`: Your mongodb uri connection url
+- `PORT`: Your desired port you want start the server
+- `JWT_SECRET`: your jwt secret code
+
+## Dependencies
+
+- `bcrypt`: Library for hashing passwords.
+- `cors`: Express middleware for enabling CORS.
+- `dotenv`: Loads environment variables from `.env` file.
+- `express`: Web framework for Node.js.
+- `jsonwebtoken`: Library for generating and verifying JWT tokens.
+- `mongodb`: MongoDB driver for Node.js.
+- `nodemon`: Utility for automatically restarting the server during development.
+
+### Live Site
+
+Live site: [here](https://multi-stage-form-for-mars-visit-application.vercel.app/).
+
+### GitHub Repository
+
+- Client: [here](https://github.com/rasel-gannicus/Multi-stage-form-for-visiting-Mars-application).
+- Server: [here](https://github.com/rasel-gannicus/Server-for-multi-stage-form-visiting-mars).
+
+### Project Previews
+
+Here are some previews of the project:
+
+*First Page*
+![Step 1](https://i.ibb.co/sC03wTL/Xnapper-2024-08-02-13-08-55.png)
+
+
+*2nd Page*
+![Step 1](https://i.ibb.co/w63ZxPq/Xnapper-2024-08-02-13-11-07.png)
+
+*3rd Page*
+![Step 1](https://i.ibb.co/K7K6gCz/Xnapper-2024-08-02-13-12-34.png)
+
+*Confirmation Page*
+![Step 1](https://i.ibb.co/9Hsh6N9/Xnapper-2024-08-02-13-13-08.png)
+
+
+*Login Page*
+![Step 1](https://i.ibb.co/zNJTkRg/Xnapper-2024-08-02-13-13-31.png)
+
+*Registration Page*
+![Step 1](https://i.ibb.co/hcR88Z6/Xnapper-2024-08-02-13-13-52.png)
---
+++ index.js
@@ -0,0 +1,145 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 2500;
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userDb = client.db("Pharmasia").collection("users");
+
+    // --- adding user data
+    app.patch("/api/v1/addUserData", async (req, res) => {
+      const userData = req.body;
+      console.log("user before : ", req.body);
+
+      const filter = { email: req.body.email };
+      const options = { upsert: true };
+
+      // Remove the _id field if it exists in the request body
+      if (userData._id) {
+        delete userData._id;
+      }
+      console.log('user after :  ', userData);
+      const update = { $set: userData };
+      try {
+        const result = await userDb.updateOne(filter, update, options);
+
+        console.log(result);
+
+        res.status(200).json({ message: "User updated successfully", result });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error updating user data", error });
+      }
+    });
+
+    //-- getting user info
+    app.get("/api/v1/userInfo/:email", async (req, res) => {
+      const { email } = req.params;
+      try {
+        const user = await userDb.findOne({ email });
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({ message: "User not found" });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving user data", error });
+      }
+    });
+
+    // User Registration
+    app.post("/api/v1/register", async (req, res) => {
+      const { name, email, password } = req.body;
+      console.log(req.body);
+
+      // Check if email already exists
+      const existingUser = await userDb.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({
+          success: false,
+          message: "User already exist !!!",
+        });
+      }
+
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Insert user into the database
+      await userDb.insertOne({
+        name,
+        email,
+        password: hashedPassword,
+        role: "user",
+      });
+
+      res.status(201).json({
+        success: true,
+        message: "User registered successfully !",
+      });
+    });
+
+    // User Login
+    app.post("/api/v1/login", async (req, res) => {
+      const { email, password } = req.body;
+
+      // Find user by email
+      const user = await userDb.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Compare hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign(
+        { email: user.email, role: user.role },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: process.env.EXPIRES_IN,
+        }
+      );
+
+      res.json({
+        success: true,
+        message: "User successfully logged in!",
+        user,
+        accessToken: token,
+      });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server running successfully !");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening from ${port}`);
+});
